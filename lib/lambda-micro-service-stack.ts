3a9e7716b5c2f42b07588a18f1be703754257c3c
Ayush Dhar <ayush_dhar@outlook.com>
2022-02-19 20:50:10 -0500

    initial

--- lib/lambda-micro-service-stack.ts
+++ lib/lambda-micro-service-stack.ts
@@ -1,16 +1,16 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps, aws_lambda, Duration } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as path from "path";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaMicroServiceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaMicroServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fn = new aws_lambda.Function(this, "backendFn", {
+      runtime: aws_lambda.Runtime.NODEJS_14_X,
+      handler: "index.handler",
+      code: aws_lambda.AssetCode.fromAsset(path.join(__dirname, "../lambda")),
+    });
   }
 }
